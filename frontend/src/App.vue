5aa6ee52fa10ca3fef22a7a3ce6419b0c40faaee
pji3504 <pji3504@naver.com>
2021-10-21 16:12:34 +0900

    build: front 개발환경 설정

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <testpage></testpage>
+  </div>
+</template>
+
+<script>
+import testpage from './components/testpage.vue'
+export default {
+  name: 'App',
+  components: {
+    testpage,
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ frontend/src/components/testpage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+      <div class=" rounded-sm border-gray-400 border-2">
+         <p class="items-center text-xl text-red-600"> 잘나오는지</p>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
