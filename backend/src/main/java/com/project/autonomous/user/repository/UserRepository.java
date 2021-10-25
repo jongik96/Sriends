eeef7b057cf2f2c78447ddc3abb0ac36c60e46bd
dnl7qjs <dnl7qjs@naver.com>
2021-10-25 17:26:24 +0900

    be_userRepository충돌 수정

--- backend/src/main/java/com/project/autonomous/user/repository/UserRepository.java
+++ backend/src/main/java/com/project/autonomous/user/repository/UserRepository.java
@@ -2,7 +2,7 @@ package com.project.autonomous.user.repository;
 
 import com.project.autonomous.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-<<<<<<< HEAD
+
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -12,9 +12,3 @@ public interface UserRepository extends JpaRepository<User, Long> {
 //    User findUserByEmail(String email);
     Optional<User> findByEmail(String email);
 }
-=======
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
->>>>>>> 50a2b1ba14286ef47887be3dca4daffc9b9e54b7
