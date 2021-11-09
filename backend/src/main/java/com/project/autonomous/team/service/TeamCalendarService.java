5e3191f3514060a92f5af774b7fb335c3a5b332a
dnl7qjs <dnl7qjs@naver.com>
2021-11-09 10:08:17 +0900

    be_api수정, 일정 수정 고침

--- backend/src/main/java/com/project/autonomous/team/service/TeamCalendarService.java
+++ backend/src/main/java/com/project/autonomous/team/service/TeamCalendarService.java
@@ -54,6 +54,8 @@ public class TeamCalendarService {
         calendar.setMonth(schedulePostReq.getSchedule().getMonthValue());
         calendar.setDay(schedulePostReq.getSchedule().getDayOfMonth());
 
+        calendarRepository.save(calendar);
+
         return true;
     }
 
