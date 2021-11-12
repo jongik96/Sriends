696b6c627238f337a243935b266bdeaf62b8a668
하지훈 <gkwlsrnjs12@gmail.com>
2021-11-12 17:22:53 +0900

    feat: backend websocket chat 구현

---
+++ backend/src/main/java/com/project/autonomous/common/config/WebSockConfig.java
@@ -0,0 +1,33 @@
+package com.project.autonomous.common.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@RequiredArgsConstructor
+@Configuration
+@EnableWebSocket
+public class WebSockConfig implements WebSocketConfigurer {
+
+//    @Override
+//    public void configureMessageBroker(MessageBrokerRegistry config) {
+//        config.enableSimpleBroker("/sub");
+//        config.setApplicationDestinationPrefixes("/pub");
+//    }
+//
+//    @Override
+//    public void registerStompEndpoints(StompEndpointRegistry registry) {
+//        registry.addEndpoint("/ws-stomp").setAllowedOrigins("*")
+//                .withSockJS();
+//    }
+
+    private final WebSocketHandler webSocketHandler;
+
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(webSocketHandler, "/ws/chat").setAllowedOrigins("*");
+    }
+}
